f3590745cef2073fb3ad819ae15df62d0057face
Divyang <teotia.d@husky.neu.edu>
2021-03-24 17:09:24 -0400

    new file

--- loader/model_loader_age.py
+++ loader/model_loader_age.py
@@ -31,34 +31,14 @@ def loadmodel(fn):
 
     # model.layer1[2].conv1.register_forward_hook(fn)
     # model.layer2[3].conv1.register_forward_hook(fn)
+    # model.layer3[2].conv1.register_forward_hook(fn)
     model.layer4[2].conv1.register_forward_hook(fn)
-    # model.layer4[2].conv1.register_forward_hook(fn)
     model.eval()
 
     return model
 
 if __name__ == '__main__':
 
-    # img = cv2.imread('../visual_dictionary/AU/SN001_3/original.png')
-    # img_copy = img.copy()
-    # img = cv2.resize(img, (128, 128))
-    # img = torch.FloatTensor(np.expand_dims(img.transpose(2, 0, 1), axis=0))
-    # print(img.shape)
-    # img2 = cv2.imread('../face_data/malekumar_env03/headrende0008.png')[:,:,::-1]
-    # print(img.shape)
-    # mtcnn = MTCNN(image_size=256)
-    # cropped1 = mtcnn(img.copy())
-    # cropped2 = mtcnn(img2.copy())
     mo = loadmodel(hook_feature)
-    # embedding = mo(img.cuda())
-    # maps = features_blobs[0]
-    # print(maps.shape)
     print(mo)
-    # for i in range(256):
-    #     cv2.imshow('i', m)
-    #     cv2.waitKey(0)
-    # print(embedding)
-    # cv2.imshow('i', img_copy)
-    # cv2.waitKey(0)
-    # embedding2 = mo(cropped2.unsqueeze(0).cuda())
-    # print(mo.layer3[1].conv2)
+
